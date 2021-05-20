91e2a52ebc9dd2330ea8c3f23b39ff619e1ed9b3
jxu477 <jxu477@gatech.edu>
2021-05-20 14:26:36 -0400

    add custome menu bar

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const electron = require("electron");
-const url = require("url");
 const path = require("path");
+const { title } = require("process");
 
 const {app, BrowserWindow } = electron;
 
 let mainWindow;
 
-app.on("ready", function() {
-    mainWindow = new BrowserWindow({ width: 600, height: 400, frame: false });
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "mainwindow.html"),
-        protocol: "file:",
-        slashes: true
-    }));
-});
+function createWindow() {
+    mainWindow = new BrowserWindow({ 
+        width: 600, 
+        height: 400, 
+        frame: false,
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js'),
+          enableRemoteModule: true,
+          nodeIntegration: true,
+        }
+    });
+    mainWindow.loadFile("./mainwindow.html");
+};
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
---
+++ preload.js
@@ -0,0 +1,12 @@
+const customTitlebar = require('custom-electron-titlebar');
+
+window.addEventListener('DOMContentLoaded', () => {
+	new customTitlebar.Titlebar({
+		backgroundColor: customTitlebar.Color.fromHex('#313233'),
+		minimizable: true,
+		maximizable: false,
+		shadow: true,
+		menu: null
+	});
+	customTitlebar.updateTitle('1');
+});
